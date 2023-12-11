1561acd9029c9ef3adf44a5d76e980151d950ea0
РР›СЋРґРјРёР»Р° РќРёР¶РµРіРѕСЂРѕРґРѕРІР° <lyudmilanizhegorodova@yandex.ru>
2023-12-11 15:41:26 +0300

    second

--- conftest.py
+++ conftest.py
@@ -13,19 +13,32 @@ def collector():
 
 
 @pytest.fixture
+def collector_add_one_book(collector):
+    """Создает словарь с одной книгой."""
+    collector.books_genre[data.one_book_name] = ''
+
+
+@pytest.fixture
 def collector_add_seven_books(collector):
     """Создает словарь с названиями книг из списка data.book_names."""
     collector.books_genre = dict.fromkeys(data.book_names, '')
 
 
 @pytest.fixture
+def collector_add_one_book_and_set_genre(collector):
+    """Создает словарь с одной книгой и присваивает ей жанр."""
+    collector.books_genre[data.one_book_name] = collector.genre[0]
+
+
+@pytest.fixture
 def collector_add_seven_books_and_set_genres(collector):
-    """Создает словарь с названиями книг из списка data.book_names и жанрами."""
+    """
+    Создает словарь с названиями книг из списка data.book_names и жанрами.
+    """
     collector.books_genre = dict(zip(data.book_names, cycle(collector.genre)))
 
 
 @pytest.fixture
-def collector_add_two_books_in_favorites(collector):
+def collector_add_one_book_in_favorites(collector, collector_add_one_book):
     """Добавляет книги из списка data.book_names в избранное."""
-    collector.favorites.append(data.book_names[0])
-    collector.favorites.append(data.book_names[1])
+    collector.favorites.append(data.one_book_name)
--- tests.py
+++ tests.py
@@ -5,78 +5,107 @@ import data
 
 class TestBooksCollector:
 
-    def test_add_new_book_no_books_added_seven_books(self, collector):
-        """Проверяет добавление новых книг."""
-        for book_name in data.book_names:
-            collector.add_new_book(book_name)
-        assert len(collector.books_genre) == len(data.book_names)
+    def test_add_new_book_no_books_added_one_books(self, collector):
+        """Проверяет добавление новой книги."""
+        collector.add_new_book('Название книги')
+        assert len(collector.books_genre) == 1
+
+    def test_add_new_book_one_books_exist_book_no_added(
+            self, collector, collector_add_one_book):
+        """Проверяет добавление уже добавленной книги."""
+        len_before_add = len(collector.books_genre)
+        collector.add_new_book(data.one_book_name)
+        len_after_add = len(collector.books_genre)
+        assert len_before_add == len_after_add
 
     @pytest.mark.parametrize(
         'name',
         [
-         'Название книги более 40 символов, а именно 45',
-         'Что делать, если ваш кот хочет вас убить'
+         '',
+         'Название книги более 40 символов: 41 симв',
+         'Название книги более 40 символов: 42 симво'
         ]
     )
-    def test_add_new_book_seven_books_not_valid_name_no_added(
-            self, collector, collector_add_seven_books, name):
-        """Проверяет добавление книг с недопустимыми названиями."""
+    def test_add_new_book_no_books_not_valid_len_name_no_added(
+            self, collector, name):
+        """Проверяет добавление книг с недопустимыми по длине названиями."""
         len_before_add = len(collector.books_genre)
         collector.add_new_book(name)
         len_after_add = len(collector.books_genre)
         assert len_before_add == len_after_add
 
-    def test_set_book_genre_seven_books_set_genres(
-            self, collector, collector_add_seven_books):
-        """Проверяет установление жанров для книг."""
-        i = 0
-        for book_name in data.book_names:
-            i = i % len(collector.genre)
-            collector.set_book_genre(book_name, collector.genre[i])
-            est_book_genre = collector.books_genre[book_name]
-            genre_in_lst = collector.genre[i]
-            i += 1
-            assert est_book_genre == genre_in_lst
-
     @pytest.mark.parametrize(
-        'name, genre',
+        'name',
         [
-            ['Название книги более 40 символов, а именно 45', 'Комедии'],
-            ['Что делать, если ваш кот хочет вас убить', 'Драмы'],
+            'Н',
+            'На',
+            'Название книги 39 символов - название--',
+            'Название книги 40 символов - название---',
         ]
     )
-    def test_set_book_genre_seven_books_not_valid_name_genre_not_set(
-            self, collector, collector_add_seven_books, name, genre):
+    def test_add_new_book_no_books_valid_len_name_added(
+            self, collector, name):
         """
-        Проверяет установление жанров: недопустимых и для отсутствующих книг.
+        Проверяет добавление книг с допустимыми граничными по длине названиями.
         """
+        len_before_add = len(collector.books_genre)
+        collector.add_new_book(name)
+        len_after_add = len(collector.books_genre)
+        assert len_before_add + 1 == len_after_add
+
+    def test_set_book_genre_one_book_set_genres(
+            self, collector, collector_add_one_book):
+        """Проверяет установление жанра для книги."""
+        collector.set_book_genre(data.one_book_name, collector.genre[0])
+        assert collector.books_genre[data.one_book_name] == collector.genre[0]
+
+    def test_set_book_genre_one_book_exist_book_not_set(
+            self, collector, collector_add_one_book):
+        """
+        Проверяет установление жанра
+        для отсутствующей в словаре books_genre книги.
+        """
+        new_name = f'Инoe {data.one_book_name}'
         len_books_genre_before_set = len(collector.books_genre)
-        len_set_genre_lst_before_set = len(list(filter(
-                lambda x: collector.books_genre[x]
-                if (name in collector.books_genre
-                    and collector.books_genre[name] != '')
-                else False, collector.books_genre)))
-        collector.set_book_genre(name, genre)
+        len_genre_lst_before_set = 0
+        for name in collector.books_genre:
+            if collector.books_genre[name] != '':
+                len_genre_lst_before_set += 1
+        collector.set_book_genre(new_name, collector.genre[0])
         len_books_genre_after_set = len(collector.books_genre)
-        len_set_genre_lst_after_set = len(list(filter(
-                lambda x: collector.books_genre[x]
-                if (name in collector.books_genre
-                    and collector.books_genre[name] != '')
-                else False, collector.books_genre)))
+        len_genre_lst_after_set = 0
+        for name in collector.books_genre:
+            if collector.books_genre[name] != '':
+                len_genre_lst_after_set += 1
         assert (len_books_genre_before_set == len_books_genre_after_set
                 and
-                len_set_genre_lst_before_set == len_set_genre_lst_after_set)
+                len_genre_lst_before_set == len_genre_lst_after_set)
 
-    def test_get_book_genre_seven_books_with_genres_got_book(
-            self, collector, collector_add_seven_books_and_set_genres):
-        """Проверяет получение жанра книги по ее нозванию."""
-        i = 0
-        for book_name in data.book_names:
-            i = i % len(collector.genre)
-            genre_for_name = collector.get_book_genre(book_name)
-            genre_in_lst = collector.genre[i]
-            i += 1
-            assert genre_for_name == genre_in_lst
+    def test_set_book_genre_one_book_not_exist_genre_not_set(
+            self, collector, collector_add_one_book):
+        """Проверяет установление отсутствующего жанра."""
+        name = data.one_book_name
+        new_genre = f'Не {collector.genre[0]}'
+        len_books_genre_before_set = len(collector.books_genre)
+        len_genre_lst_before_set = 0
+        for name in collector.books_genre:
+            if collector.books_genre[name] != '':
+                len_genre_lst_before_set += 1
+        collector.set_book_genre(name, new_genre)
+        len_books_genre_after_set = len(collector.books_genre)
+        len_genre_lst_after_set = 0
+        for name in collector.books_genre:
+            if collector.books_genre[name] != '':
+                len_genre_lst_after_set += 1
+        assert (len_books_genre_before_set == len_books_genre_after_set
+                and
+                len_genre_lst_before_set == len_genre_lst_after_set)
+
+    def test_get_book_genre_one_book_with_genre_got_genre(
+            self, collector, collector_add_one_book_and_set_genre):
+        """Проверяет получение жанра книги по ее названию."""
+        genre_for_name = collector.get_book_genre(data.one_book_name)
+        assert genre_for_name == collector.books_genre[data.one_book_name]
 
     def test_get_books_with_specific_genre_seven_books_with_genres_got_books(
             self, collector, collector_add_seven_books_and_set_genres):
@@ -90,8 +119,8 @@ class TestBooksCollector:
                        collector.books_genre))
             assert lst_books_with_genre == books_lst
 
-    def test_get_books_genre_seven_books_with_genres_got_books_genre(
-            self, collector, collector_add_seven_books_and_set_genres):
+    def test_get_books_genre_one_book_with_genres_got_books_genre(
+            self, collector, collector_add_one_book_and_set_genre):
         """Проверяет вывод словаря с названиями и жанрами книг."""
         assert collector.get_books_genre() == collector.books_genre
 
@@ -111,35 +140,39 @@ class TestBooksCollector:
         temp_lst.sort()
         assert temp_lst == books_lst_without_age_rating
 
-    def test_add_book_in_favorites_seven_books_added_book(
-            self, collector, collector_add_seven_books):
-        """Проверяет добавление новых книг в избранное."""
-        value = list(collector.books_genre.keys())[0]
-        collector.add_book_in_favorites(value)
-        assert data.book_names[0] in collector.favorites
+    def test_get_books_for_children_seven_books_with_genres_not_got_books_with_genre_age_rating(
+            self, collector, collector_add_seven_books_and_set_genres):
+        """
+        Проверяет отсутствие в списке книг для детей
+        книг с возрастным рейтингом.
+        """
+        temp_lst = collector.get_books_for_children()
+        for name in collector.books_genre:
+            if collector.books_genre[name] in collector.genre_age_rating:
+                assert collector.books_genre[name] not in temp_lst
 
-    @pytest.mark.parametrize('name',
-                             ['Что делать, если ваш кот хочет вас убить'])
-    def test_add_book_in_favorites_two_books_already_added_book_not_added(
-            self, collector, collector_add_seven_books,
-            collector_add_two_books_in_favorites, name):
-        """Проверяет повторное добавление книг в избранное."""
+    def test_add_book_in_favorites_one_book_added_book(
+            self, collector, collector_add_one_book):
+        """Проверяет добавление новой книги в избранное."""
+        collector.add_book_in_favorites(data.one_book_name)
+        assert data.one_book_name in collector.favorites
+
+    def test_add_book_in_favorites_one_book_already_added_book_not_added(
+            self, collector, collector_add_one_book_in_favorites):
+        """Проверяет повторное добавление книги в избранное."""
         len_favorites_before_add = len(collector.favorites)
-        collector.add_book_in_favorites(name)
+        collector.add_book_in_favorites(data.one_book_name)
         len_favorites_after_add = len(collector.favorites)
         assert len_favorites_before_add == len_favorites_after_add
 
-    def test_delete_book_from_favorites_seven_books_deleted_book(
-            self, collector, collector_add_seven_books,
-            collector_add_two_books_in_favorites):
+    def test_delete_book_from_favorites_one_book_deleted_book(
+            self, collector, collector_add_one_book_in_favorites):
         """Проверяет удаление книги из избранного."""
-        value = collector.favorites[0]
-        collector.delete_book_from_favorites(value)
-        assert data.book_names[0] not in collector.favorites
+        collector.delete_book_from_favorites(data.one_book_name)
+        assert data.one_book_name not in collector.favorites
 
     def test_get_list_of_favorites_books_seven_books_got_favorites(
-            self, collector, collector_add_seven_books,
-            collector_add_two_books_in_favorites):
+            self, collector, collector_add_seven_books):
         """Проверяет вывод списка книг из избранного."""
         value = collector.books_genre.keys()
         collector.favorites.append(list(value)[0])
